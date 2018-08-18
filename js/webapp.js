a84a583e3f9f6f0eac8d888e4c811a984e6c2d20
RealRedChief <37128377+RealRedChief@users.noreply.github.com>
2018-08-18 06:43:20 -0500

    Navbar design change
    navbar changes to white when off canvas menu toggled

--- js/webapp.js
+++ js/webapp.js
@@ -1,8 +1,12 @@
 const menu = document.querySelector('.naver');
 const intro = document.querySelector('.intro');
+const fa = document.querySelector('i');
+
+
 menu.addEventListener('click', togglme);
 
 function togglme() {
 intro.classList.toggle('active');
+fa.classList.toggle('white');
 
 }
