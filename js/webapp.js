3cf068380e99be6b46ae9f51f8e8f840585c4714
Marc Dwyer <envdia@gmail.com>
2018-08-18 09:33:43 -0500

    More off canvas optimizations
    if innerwidth >= 775 toggle nav class

--- js/webapp.js
+++ js/webapp.js
@@ -3,10 +3,20 @@ const intro = document.querySelector('.intro');
 const fa = document.querySelector('i');
 
 
-menu.addEventListener('click', togglme);
 
-function togglme() {
+
+menu.addEventListener('click', toggleme);
+
+function toggleme() {
 intro.classList.toggle('active');
 fa.classList.toggle('white');
+}
 
+intro.addEventListener('transitionend', endcanvas);
+
+function endcanvas(e) {
+  console.log(this.classList)
+  if (this.classList.value.includes('active') && window.innerWidth >= 775) {
+    intro.classList.toggle('active');
+  }
 }
