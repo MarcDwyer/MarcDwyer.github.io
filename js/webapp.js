9b0a97cfec1f0dd39cdc259de2b2ea10ccca42ef
Marc Dwyer <envdia@gmail.com>
2018-08-20 15:07:07 -0500

    Revert some changes

--- js/webapp.js
+++ js/webapp.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.naver');
 const intro = document.querySelector('.intro');
 const fa = document.querySelector('i');
+const content = document.querySelector('.content');
 
 
 menu.addEventListener('click', toggleme);
@@ -8,15 +9,15 @@ menu.addEventListener('click', toggleme);
 function toggleme() {
 
 intro.classList.toggle('active');
-fa.classList.toggle('white');
+menu.classList.toggle('white');
 }
 
 intro.addEventListener('transitionend', endcanvas);
 
 function endcanvas(e) {
   if (e.propertyName == 'background-color' && window.innerWidth >= 775) {
-    intro.classList.toggle('active');
-    fa.classList.toggle('white');
+    intro.classList.remove('active');
+    menu.classList.toggle('white');
   }
 }
 
@@ -29,20 +30,5 @@ function unicorn(e) {
   empty.splice(-secret.length - 1, empty.length - secret.length);
   if (empty.join('').includes(secret)) {
     cornify_add();
-  }
-}
-
-
-let last = window.pageYOffset;
-
-window.onscroll = function () {
-  if (window.innerWidth <= 780) {
-    const current = window.pageYOffset;
-    if (last >= current) {
-      menu.style.top = "0";
-    } else {
-      menu.style.top = "-50px";
-    }
-    last = current;
   }
 }
